9d7a8ee0526d6cd05c386591dc476aa8c22c888a
Prashant Kumar <44938735+Prashant517@users.noreply.github.com>
2025-05-07 19:46:17 +0530

    find all unique triplets (group of 3 numbers) such that their sum is 0.

--- Array/sum_of_triple_value_using_two_pointers.js
+++ Array/sum_of_triple_value_using_two_pointers.js
@@ -47,37 +47,4 @@
 // Crio Methodology - Problem Approach
 // Brainstorm some approaches for solving the problem before viewing the video
 
-console.log("Using Bruteforce Method");
-
-const startTime = performance.now(); 
-
-var loopCount = 0;
-const nums = [-3, 0, 1, 2, -1, 1, -2];
-const uniqueVal = new Set();
-const n = nums.length;
-const result = [];
-
-for (let i = 0; i < n; i++){
-    loopCount++;
-    for (let j = i+1; j < n; j++){
-        loopCount++;
-        for (let k = j+1; k < n; k++){
-            loopCount++;
-            var sum = nums[i] + nums[j] + nums[k];
-            if (sum === 0){
-                const triplet = [nums[i], nums[j], nums[k]].sort((a, b) => a - b);
-                const val = triplet.join(','); // Convert to string val
-                if (!uniqueVal.has(val)) {
-                    result.push([nums[i], nums[j], nums[k]]);
-                    uniqueVal.add(val); // store into uniqueVal to check next val should not same
-                }
-            }
-        }
-    }
-}
-
-const endTime = performance.now(); // End the timer
-
-console.log(`Execution Time: ${(endTime - startTime).toFixed(2)}ms`);
-console.log("loopCount : "+  loopCount);
-result.forEach(triplet => console.log(triplet.join(' ')));
+console.log("Using Two Pointers Method");
